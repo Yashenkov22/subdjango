220e64f766cf2e77452dba31b851e906ac54a24e
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-03-11 17:38:37 +0300

    test: add log file

--- project/project/settings.py
+++ project/project/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1',
